39b82bf66b7aa0450db31c8caad8aa6dd48c0d44
liliu <liliu@iboxpay.com>
2017-01-11 17:03:43 +0800

    checkbox

--- examples/pages/checkbox.vue
+++ examples/pages/checkbox.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
     <vui-header fixed>Checkbox</vui-header>
+    <page-content>
+      <vui-checkbox v-model="checkboxValue.one" value="one">One</vui-checkbox>
+      <vui-checkbox v-model="checkboxValue.two" value="two" disabled>Two (disabled)</vui-checkbox>
+      <vui-checkbox v-model="checkboxValue.three" value="three" readonly>Two (readonly)</vui-checkbox>
+
+    </page-content>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      checkboxValue: {
+        one: null,
+        two: 'two',
+        three: null
+      }
+    }
+  }
 }
 </script>
---
+++ src/components/checkbox.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input type="checkbox" id="checkbox_a" class="chk_1">
+    <label for="checkbox_a">check<i class="icon icon-checkbox-unchecked"></i></label>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vui-checkbox',
+  data: function() {
+    return {
+      icons: ['checkbox-unchecked', 'checkbox-checked', 'checkbox-checked-fill']
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chk_1 {
+  display: none;
+}
+.chk_1 + label:before {
+  content: '\e720';
+  display: inline-block;
+  position: relative;
+}
+.chk_1:active + label:before {
+  content: '\e721';
+  display: inline-block;
+  position: relative;
+}
+.chk_1:checked + label:before {
+  content: '\e722';
+  display: inline-block;
+  position: relative;
+}
+</style>
